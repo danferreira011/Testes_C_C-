084e84a13df1addb48d18596487cebd53190c33f
Danilo Ferreira <danilosantos.turbo@gmail.com>
2024-05-01 13:07:02 -0300

    Initial commit

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void troca(int a, int b) {
+ int temp = a;
+ a = b;
+ b = temp;
+}
+int main() {
+ int x = 10, y = 20;
+ troca(x, y); 
+ printf("x=%d, y=%d\n", x, y);
+ return 0;
+}
